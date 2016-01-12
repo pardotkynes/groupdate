0ae6a2cb677b8e2647b715d5c2651e60644d2590
pardotkynes <support@mimiran.com>
2016-01-12 14:47:03 -0600

    Changes for older version of Rails

--- lib/groupdate/active_record.rb
+++ lib/groupdate/active_record.rb
@@ -7,12 +7,21 @@ ActiveRecord::Base.send(:extend, Groupdate::Scopes)
 
 module ActiveRecord
   class Relation
+    
+    # add this to avoid method missing error
+    def reverse_order_value
+      false #  no idea if this is right...
+    end
+    
     if ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR < 2
 
       def method_missing_with_hack(method, *args, &block)
+        puts "MISSING METHOD: #{method.inspect}"
         if Groupdate::METHODS.include?(method)
+          puts "Sending in GroupDate..."
           scoping { @klass.send(method, *args, &block) }
         else
+          puts "Method missing without hack....."
           method_missing_without_hack(method, *args, &block)
         end
       end
@@ -26,7 +35,9 @@ module ActiveRecord
   module Associations
     class CollectionProxy
       if ActiveRecord::VERSION::MAJOR == 3
-        delegate *Groupdate::METHODS, :to => :scoped
+
+        delegate Groupdate::METHODS, :to => :scoped
+        
       end
     end
   end
@@ -39,9 +50,11 @@ module ActiveRecord
     private
 
     def column_alias_for_with_hack(*keys)
-      if keys.first.is_a?(Groupdate::OrderHack)
+      if keys.first.is_a?(Groupdate::OrderHack) && false
+        
         keys.first.field
       else
+        
         column_alias_for_without_hack(*keys)
       end
     end
